2e3b840d441d3c4f2d9242ee7e85472234926a0e
judit <judit@fedora>
2022-05-26 18:43:01 +0200

    Registration system completed

--- Test/src/application/RegisterController.java
+++ Test/src/application/RegisterController.java
@@ -1,6 +1,11 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,6 +14,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -19,7 +25,7 @@ public class RegisterController {
 	private String mail;
 	private String city;
 	private String password;
-	
+
 	@FXML TextField nameTField;
 	@FXML TextField mailTField;
 	@FXML TextField cityTField;
@@ -27,15 +33,56 @@ public class RegisterController {
 	@FXML PasswordField passwordPField;
 	@FXML Button goLogin;
 	@FXML Button checkRegister;
+	@FXML Label errorUserExists;
 	//--
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-	
-	
-	
-	
-	
+
+
+	public void checkAccount(ActionEvent event) throws SQLException, IOException {
+		name = nameTField.getText();
+		surname = surnameTField.getText();
+		mail = mailTField.getText();
+		city = cityTField.getText();
+		password = passwordPField.getText();
+		boolean canRegister = true;
+		String maxUID = null;
+
+		Connection myConnection=null;
+		try {
+			myConnection = DriverManager.getConnection("jdbc:mysql://192.168.1.99:3306","judit","1234");
+		} catch (SQLException e) {
+			myConnection = DriverManager.getConnection("jdbc:mysql://92.178.96.124:3306","judit","1234");
+			System.out.println("ee");
+			e.printStackTrace();
+
+		}
+
+		Statement myStatement = myConnection.createStatement();
+		ResultSet lastUID = myStatement.executeQuery("SELECT MAX(UserId) FROM HomeIn.User");
+		while(lastUID.next()) {
+			maxUID = lastUID.getString(1); 
+		}
+		maxUID = Integer.toString(Integer.parseInt(maxUID)+1); 
+		ResultSet userExists = myStatement.executeQuery("SELECT Mail FROM HomeIn.User");
+		while(userExists.next()) {
+			if(userExists.getString(1).equals(mail) ) {
+				canRegister = false;
+			}
+		}
+		if(!canRegister) {
+			errorUserExists.setText("User already exists!");
+		}else {
+			myStatement.executeUpdate("INSERT INTO HomeIn.User (UserId,Name,LastName,Mail,City,Password) VALUES('"
+					+maxUID+"', '"+name+"', '"+surname+"', '"+mail+"', '"+city+"', '"+password+"')");
+			toLogin(event);
+		}
+
+	}
+
+
+
 	//Switch to main page
 	public void toLogin(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
@@ -44,6 +91,6 @@ public class RegisterController {
 		stage.setScene(scene);
 		stage.show();
 	}
-	
-	
+
+
 }
