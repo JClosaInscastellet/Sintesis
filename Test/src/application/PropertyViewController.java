1d43576b39e963d6aee39540ee9f8992d6e17251
judit.mendo <jmendozam@inscastellet.cat>
2022-06-01 19:41:25 +0200

    D

--- Test/src/application/PropertyViewController.java
+++ Test/src/application/PropertyViewController.java
@@ -3,10 +3,12 @@ package application;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class PropertyViewController {
@@ -15,6 +17,17 @@ public class PropertyViewController {
 	private Scene scene;
 	private Parent root;
 	public static int pToShow;
+	
+	@FXML Label room;
+	@FXML Label bathroom;
+	@FXML Label surface;
+	@FXML Label price;
+	@FXML Label city;
+	@FXML Label zone;
+	@FXML Label parking;
+	@FXML Label elevator;
+	
+	
 	public void pullBack(ActionEvent event) throws IOException {
 		
 		root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
